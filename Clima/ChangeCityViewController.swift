117030fbcdbdcfe1620d38db6df72407ccfb2de9
Charles Xie <charlesxie@charless-macbook-pro.local>
2019-02-11 13:45:49 -0800

    Update this.

--- Clima/ChangeCityViewController.swift
+++ Clima/ChangeCityViewController.swift
@@ -10,13 +10,20 @@ import UIKit
 
 
 //Write the protocol declaration here:
+protocol ChangeSettingDelegate {
+    func userEnteredANewCityName(city: String)
 
-
+    func userToggled(isToggled : String)
+    //this function in the protocol allows the changeCityViewController to pass information through the delegate
+}
 
 class ChangeCityViewController: UIViewController {
     
+    var featureOnOrOff = ""
+    //this variable keeps track of whether the toggle is on or off. This will be used when changeCityViewController passes information to the weatherViewController
+    
     //Declare the delegate variable here:
-
+    var delegate : ChangeSettingDelegate?
     
     //This is the pre-linked IBOutlets to the text field:
     @IBOutlet weak var changeCityTextField: UITextField!
@@ -25,24 +32,37 @@ class ChangeCityViewController: UIViewController {
     //This is the IBAction that gets called when the user taps on the "Get Weather" button:
     @IBAction func getWeatherPressed(_ sender: AnyObject) {
         
-        
-        
         //1 Get the city name the user entered in the text field
+        let cityName = changeCityTextField.text!
         
         
         //2 If we have a delegate set, call the method userEnteredANewCityName
-        
+        delegate?.userEnteredANewCityName(city: cityName)
         
         //3 dismiss the Change City View Controller to go back to the WeatherViewController
-        
-        
-    }
-    
-    
-
-    //This is the IBAction that gets called when the user taps the back button. It dismisses the ChangeCityViewController.
-    @IBAction func backButtonPressed(_ sender: AnyObject) {
         self.dismiss(animated: true, completion: nil)
     }
     
+
+    @IBAction func toggleSunriseSunset(_ sender: UISwitch) {
+        if (sender.isOn == true){
+            featureOnOrOff = "isOn"
+            delegate?.userToggled(isToggled: featureOnOrOff)
+        //if the toggle is on, we should call a method on the delegate to alert the weatherViewController to display sunrise/sunset times. featureOnorOff keeps track of whether the toggle is on or off
+        }else{
+            featureOnOrOff = "isOff"
+            delegate?.userToggled(isToggled: featureOnOrOff)
+        //if the toggle is off,we should call a method on the delegate to alert the weatherViewController to not display sunrise/sunset times.
+       
+        }
+    }
+    
+
+    //when the user taps the back button, this function dismisses the ChangeCityViewController.
+   @IBAction func backButtonPressed(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    
+    }
+    
+
 }
--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -7,33 +7,43 @@
 //
 
 import UIKit
+import CoreLocation
+import Alamofire
+import SwiftyJSON
 
-
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate, ChangeSettingDelegate{
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
     let APP_ID = "e72ca729af228beabd5d20e3b7749713"
     /***Get your own App ID at https://openweathermap.org/appid ****/
     
-
+    
     //TODO: Declare instance variables here
+    let locationManager = CLLocationManager()
+    let weatherDataModel = WeatherDataModel()
+   
+    var tempDate = ""
+    var tempDate2 = ""
+    //the variables to store the sunset and sunrise time
     
-
     
-    //Pre-linked IBOutlets
+    //IBOutlets
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-
+    @IBOutlet weak var sunriseLabel: UILabel!
+    @IBOutlet weak var sunsetLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        //TODO:Set up the location manager here.
-    
-        
+        //Setting up the location manager
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
     }
     
@@ -44,7 +54,21 @@ class WeatherViewController: UIViewController {
     
     //Write the getWeatherData method here:
     
-
+    func getWeatherData(url : String, parameters : [String:String]) {
+        Alamofire.request(url, method : .get, parameters : parameters).responseJSON {
+            response in
+            if response.result.isSuccess {
+        
+                
+                let weatherJSON : JSON = JSON(response.result.value!)
+                self.updateWeatherData(json: weatherJSON)
+            }
+            else{
+                self.cityLabel.text = "Connection Issues"
+            }
+        }
+        
+    }
     
     
     
@@ -52,11 +76,52 @@ class WeatherViewController: UIViewController {
     
     //MARK: - JSON Parsing
     /***************************************************************/
-   
+    
     
     //Write the updateWeatherData method here:
     
+    //This is the function to convert unix time to current time
+    func getDate(unixdate: Int) -> String {
+        let date = NSDate(timeIntervalSince1970: TimeInterval(unixdate))
+        let dayTimePeriodFormatter = DateFormatter()
+        dayTimePeriodFormatter.dateFormat = "hh:mm"
+        let dateString = dayTimePeriodFormatter.string(from: date as Date)
+        return "\(dateString)"
+        
+    }
+    
+    func updateWeatherData(json: JSON){
+        if let tempResult = json["main"]["temp"].double {
+            weatherDataModel.temperature = Int(tempResult - 237.15)
+            weatherDataModel.city = json["name"].stringValue
+            weatherDataModel.condition = json["weather"][0]["id"].intValue
+            weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition:weatherDataModel.condition)
+            //this code parses the JSON file to search for the temperature (in kelvins)
+        }
+        else {
+            cityLabel.text = "Weather Unavailable"
+        }
+        
+        weatherDataModel.sunrise = json["sys"]["sunrise"].intValue
+        weatherDataModel.sunset = json["sys"]["sunset"].intValue
+        //this code parses the JSON file to search for the sunrise and sunset times
+        
 
+   tempDate = getDate(unixdate: weatherDataModel.sunrise)
+      print(tempDate)
+     sunriseLabel.text = tempDate
+    //converting sunrise unix time to current time
+        
+        
+       
+    tempDate2 = getDate(unixdate: weatherDataModel.sunset)
+        print(tempDate2)
+        sunsetLabel.text = tempDate2
+      //converting sunset unix time to current time
+        
+        
+        updateUIWithWeatherData()
+    }
     
     
     
@@ -65,8 +130,12 @@ class WeatherViewController: UIViewController {
     
     
     //Write the updateUIWithWeatherData method here:
-    
-    
+   //this is the function that updates the labels that display the city and temperature
+    func updateUIWithWeatherData() {
+        cityLabel.text = weatherDataModel.city
+        temperatureLabel.text = "\(weatherDataModel.temperature)°"
+        weatherIcon.image = UIImage(named:weatherDataModel.weatherIconName)
+    }
     
     
     
@@ -76,29 +145,60 @@ class WeatherViewController: UIViewController {
     
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count-1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+        }
+        print("longitude = \(location.coordinate.longitude), latitude = \(location.coordinate.latitude)")
+        let latitude = String(location.coordinate.latitude)
+        let longitude = String(location.coordinate.longitude)
+        let params : [String:String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+        
+        getWeatherData(url: WEATHER_URL, parameters: params)
+    }
     
     
     //Write the didFailWithError method here:
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
     
     
-
     
     //MARK: - Change City Delegate methods
     /***************************************************************/
     
     
     //Write the userEnteredANewCityName Delegate method here:
+    func userEnteredANewCityName(city: String) {
+        let params : [String : String] = ["q" : city, "appid" : APP_ID]
+        getWeatherData(url: WEATHER_URL, parameters: params)
+    }
     
-
+    //this is the function that updates the sunrise sunset times based on whether the toggle is on or off
+    func userToggled(isToggled : String) {
+        if isToggled == "isOn"{
+            sunriseLabel.text = tempDate
+            sunsetLabel.text = tempDate2
+            //if toggle is on, the labels should display tempDate or tempDate2
+        }
+        if isToggled == "isOff"{
+            sunriseLabel.text = ""
+            sunsetLabel.text = ""
+            // if toggle is off, the label should be empty
+        }
+    }
     
     //Write the PrepareForSegue Method here
-    
-    
-    
-    
-    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "changeCityName" {
+            let destinationVC = segue.destination as! ChangeCityViewController
+            destinationVC.delegate = self
+        
+        }
+    }
 }
-
 
